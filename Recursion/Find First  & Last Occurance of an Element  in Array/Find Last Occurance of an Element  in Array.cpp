58f12dd52c8336962709f8a05505e5bd93afc1ab
Samius Sazin <68127599+Samius-Sazin@users.noreply.github.com>
2023-05-02 20:06:04 +0600

    Update Find Last Occurance of an Element  in Array.cpp

--- Recursion/Find First  & Last Occurance of an Element  in Array/Find Last Occurance of an Element  in Array.cpp
+++ Recursion/Find First  & Last Occurance of an Element  in Array/Find Last Occurance of an Element  in Array.cpp
@@ -6,6 +6,20 @@
 #include<iostream>
 using namespace std;
 
+//Method 1:
+int last_occurance(int *arr, int n, int i, int key)
+{
+    if(n == i) return -1;
+    int index = last_occurance(arr, n, i+1, key);
+
+    if(index != -1) return index;
+
+    if(arr[i] == key) return i;
+
+    return -1;
+}
+
+//Method 2:
 int last_occurance(int *arr, int n, int key)
 {
     if(n == 0) return -1;
@@ -25,15 +39,15 @@ int main()
     n = sizeof(arr)/sizeof(int);
 
     for(int i=0; i<n; i++)
-    cout<<arr[i]<<" ";
+        cout<<arr[i]<<" ";
     cout<<"\nEnter Element to found : ";
     cin>>key;
 
-    res = last_occurance(arr, n, key);
+    //res = last_occurance(arr, n, key);
+    res = last_occurance(arr, n, 0, key);
 
     if(res != -1) cout<<"Element Found at Position : "<<res<<endl;
     else cout<<"Element Not Found"<<endl;
 
     return 0;
 }
-
