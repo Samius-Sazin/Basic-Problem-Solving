40032fa9c70112db1f0c3e21faaedcf13c6c515d
Samius Sazin <68127599+Samius-Sazin@users.noreply.github.com>
2023-08-17 16:22:32 +0600

    Create goto_Conditional_Statement.cpp

---
+++ goto_Conditional_Statement.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 1;
+
+    level_print:
+    cout << a << " ";
+    a++;
+    
+    if(a <= 5){
+        goto level_print;
+    }
+}
+//Print : 1 2 3 4 5
